38540300c90f145619ca3cc66b492afa1cc92e4d
심정민 <shjj09@naver.com>
2020-08-12 21:35:24 +0900

    add addRole function

--- src/main/java/kr/or/connect/djbcexam/dao/RoleDao.java
+++ src/main/java/kr/or/connect/djbcexam/dao/RoleDao.java
@@ -13,6 +13,49 @@ public class RoleDao {
 	public static String dbUser = "root";
 	public static String dbpasswd = "sjm1771033";
 	
+	public int addRole(Role role) {
+		int insertCount = 0;
+		Connection conn = null;
+		PreparedStatement ps = null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			conn = (Connection) DriverManager.getConnection(dburl, dbUser, dbpasswd);
+			
+			String sql = "INSERT INTO role (role_id, description) VALUES (?, ?)";
+			
+			ps = (PreparedStatement) conn.prepareStatement(sql);
+			
+			ps.setInt(1, role.getRoleId());
+			ps.setString(2, role.getDescription());
+			
+			insertCount = ps.executeUpdate();
+			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			//먼저 닫아주기 
+			//반드시 수행되는 구절 
+			if(ps != null) {
+				try {
+					ps.close();
+				} catch (Exception e2) {
+					e2.printStackTrace();
+				}
+			}
+			if(conn != null) {
+				try {
+					conn.close();
+				} catch (Exception e2) {
+					e2.printStackTrace();
+				}
+			}
+		}
+		
+		return insertCount;
+	}
+	
 	public Role getRole(Integer roleId) {
 		Role role = null;
 		Connection conn = null;
@@ -45,14 +88,14 @@ public class RoleDao {
 			}
 			if(ps != null) {
 				try {
-					rs.close();
+					ps.close();
 				} catch (Exception e2) {
 					e2.printStackTrace();
 				}
 			}
 			if(conn != null) {
 				try {
-					rs.close();
+					conn.close();
 				} catch (Exception e2) {
 					e2.printStackTrace();
 				}
